61d9c41e893ce1c0b2edba0199653cfacea2d1d5
Azer Agazade <azer.agazade05@gmail.com>
2024-02-27 22:40:32 +0400

    add lesson4:task4

---
+++ Lesson04/Task04/Program.cs
@@ -0,0 +1,51 @@
+﻿string GetNumbers(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == false)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+int GetSizeOfArray(int num)
+{
+    int size = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        size++;
+    }
+    return size;
+}
+
+int[] FillArray(int num, int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[size - 1 - i] = num % 10;
+        num /= 10;
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach(int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+string str = Console.ReadLine();
+string result = GetNumbers(str);
+Console.WriteLine(result);
+int number = Convert.ToInt32(result);
+Console.WriteLine(number);
+int size = GetSizeOfArray(number);
+int[] arrayFromConsole = FillArray(number, size);
+PrintArray(arrayFromConsole);
