81493bdc61966f181f26b3fac72215025eddeb46
Azer Agazade <azer.agazade05@gmail.com>
2024-03-09 15:32:26 +0400

    add lesson5

---
+++ Lesson05/Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿long FindFactorial(int number)
+{
+    if (number == 1 || number == 0)
+    {
+        Console.WriteLine($"Stop: {number}");
+        return 1;
+    }
+    Console.WriteLine(number);
+    return number * FindFactorial(number - 1);
+}
+
+Console.Write(FindFactorial(5));
+ 
